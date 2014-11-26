c4efa628c239cf3814fd8ee49541530941a3f533
Lichun Wei <lichun.william@gmail.com>
2014-11-26 12:11:41 +0800

    ignore '#' in host file

--- src/query_hosts.py
+++ src/query_hosts.py
@@ -4,7 +4,7 @@
 '''
 Author      : Wei Lichun<lichun.william@gmail.com>
 Create Date : Sat Sep 10 18:58:31 CST 2011
-Version     : 0.1
+Version     : 0.2
 
 This is free software; see the source for copying conditions. 
 There is NO warranty; not even for MERCHANTABILITY or FITNESS
@@ -16,16 +16,26 @@ import subprocess
 import expand_range
 
 def get_hosts_from_file(filename=''):
+  """This function parses the contents of a stream to generate a list of lines.
+  This function removes comments delimited by '#', ingores empty
+  lines, and leading and trailing whitespace.
+  Returns:
+     a list of hosts
+  """
     f=open(filename)
     result=[]
     while(True):
-        host=f.readline().replace('\n','')
-        if host:
-            result.append( host.strip())
+        line=f.readline().replace('\n','')
+        if line:
+            idx = line.find('#')
+            if idx >= 0:
+                line = line[:idx]
+            if line:
+                result.append( line.strip())
         else:
             break
     return result
-
+    
 def get_hosts_from_range(range=''):
     if(range):
         return expand_range.expand(range)
